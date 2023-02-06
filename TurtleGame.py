8e408ca09f8a3a9978e84a5296d43658512df1b0
Moises Martinez <moises@martinez.sh>
2023-02-06 19:47:28 +0100

    Refactored the entire code to use objects and get rid of global variables

--- TurtleGame.py
+++ TurtleGame.py
@@ -3,17 +3,86 @@ import random
 import datetime
 from datetime import timedelta
 
-win = Screen()
-win.colormode(255)
-win.bgcolor(0, 0, 255)
+class GameState:
+    def __init__(self):
+        self.gameover = False
+        self.game_timer = 0
+        self.refresh_text_timer = 0
+        self.previous_time = datetime.datetime.now()
+        self.text = Turtle()
+        self.text.penup()
+        self.text.setposition((0,350))
+        self.text.write(arg="{}".format(120 - int(self.game_timer/1000)),font=("Arial", 64, "normal"))
+        self.players = []
+        self.characters = []
+        self.player_speed = 15
+        self.screen_size = None
+        
+    def update(self):
+        current_time = datetime.datetime.now()
+        delta = current_time - self.previous_time
+        self.refresh_text_timer = self.refresh_text_timer + delta.total_seconds()*1000
+        self.game_timer = self.game_timer + delta.total_seconds()*1000
+        self.previous_time = current_time
+        
+        if self.refresh_text_timer > 1000:
+            self.refresh_text_timer = 0
+            if not self.gameover:
+                self.text.clear()
+                self.text.write(arg="{}".format(120 - int(self.game_timer/1000)),font=("Arial", 64, "normal"))
+        
+        if self.game_timer > 120000:
+            self.game_timer = 0
+            self.text.clear()
+        
+            winner = "player 2"
+        
+            if self.players[0].bias > self.players[1].bias:
+                winner = "player 1"
+            elif self.players[0].bias == self.players[1].bias:
+                winner = "nobody"
+        
+            self.text.write(arg="The winner is {}".format(winner),font=("Arial", 64, "normal"))
+            
+            self.gameover = True
+            
+    def update_players_position(self):
+        for player in self.players:
+            if player.state["direction"] == "up":
+                player.forward(self.player_speed)
+            if player.state["direction"] == "down":
+                player.back(self.player_speed)
+            if player.state["direction"] == "left":
+                player.left(self.player_speed)
+            if player.state["direction"] == "right":
+                player.right(self.player_speed)
 
-screen_size = win.screensize()
+    def update_collisions(self):
+        p1_pos_x = self.players[0].pos()[0]
+        p1_pos_y = self.players[0].pos()[1]
+        p2_pos_x = self.players[1].pos()[0]
+        p2_pos_y = self.players[1].pos()[1]
+        food_pos_x = self.characters[2].pos()[0]
+        food_pos_y = self.characters[2].pos()[1]
+        
+        if self.players[0].bias >= abs(p1_pos_x - food_pos_x) and self.players[0].bias >= abs(p1_pos_y - food_pos_y):
+            current_size = self.players[0].get_size()
+            self.players[0].resize(current_size[0]+1,current_size[1]+1,current_size[2])
+            self.players[0].bias = self.players[0].bias + 5
+            self.characters[2].setposition(random.randrange(-1*self.screen_size[0]*0.5,self.screen_size[0]*0.5),\
+                            random.randrange(-1*self.screen_size[1]*0.5,self.screen_size[1]*0.5))   
 
-players = []
-characters = []
+        if self.players[1].bias >= abs(p2_pos_x - food_pos_x) and self.players[1].bias >= abs(p2_pos_y - food_pos_y):
+            current_size = self.players[1].get_size()
+            self.players[1].resize(current_size[0]+1,current_size[1]+1,current_size[2])
+            self.players[1].bias = self.players[1].bias + 5
+            self.characters[2].setposition(random.randrange(-1*self.screen_size[0]*0.5,self.screen_size[0]*0.5),\
+                            random.randrange(-1*self.screen_size[1]*0.5,self.screen_size[1]*0.5)) 
+
+        
 
 class Player:
-    def __init__(self,color,position,players,characters,):
+    def __init__(self,color,position):
         self.bias = 7
         self.state = { "direction": "", "key_pressed": False }
         self.player = Turtle()
@@ -21,10 +90,11 @@ class Player:
         self.player.color(color[0],color[1],color[2])
         self.player.penup()
         self.player.setposition(position[0],position[1])
-        
-
-        players.append(self)
-        characters.append(self)
+        self.eaten = 0
+                
+    def reset(self):
+        self.eaten = 0
+        self.bias = 7
 
     def pos(self):
         return self.player.pos()
@@ -73,134 +143,46 @@ class Player:
         self.state["direction"] = ""
         self.state["key_pressed"] = False
             
+class TurtleGame:
+    def __init__(self):
+        self.win = Screen()
+        self.win.colormode(255)
+        self.win.bgcolor(0, 0, 255)
         
-        
+        self.screen_size = self.win.screensize()
+                
+        self.state = GameState()
+        self.state.screen_size = self.screen_size
 
+        p1 = Player((100,0,125),(0,0))
+        p2 = Player((0,255,2),(30,0))
 
-def createFood(position):
-    food = Turtle()
-    food.shape("circle")
-    food.setposition(position[0],position[1])
+        self.state.players.append(p1)
+        self.state.players.append(p2)
+        self.state.characters.append(p1)
+        self.state.characters.append(p2)
 
-    characters.append(food)
+        p1.set_keys(self.win,["w","s","a","d"])
+        p2.set_keys(self.win,["i","k","j","l"])
 
-p1 = Player((100,0,125),(0,0),players,characters)
-p2 = Player((0,255,2),(30,0),players,characters)
+        food = Turtle()
+        food.shape("circle")
+        food.setposition((0,200))
+        self.state.characters.append(food)
 
-p1.set_keys(win,["w","s","a","d"])
-p2.set_keys(win,["i","k","j","l"])
-
-createFood((0,0))
-
-p1_key_state = { "direction": "", "key_pressed": False }
-p2_key_state = { "direction": "", "key_pressed": False }
-player_speed = 15
-sensetivity = 15
-
-previous_text = None
-
-def speed1():
-    global player_speed
-    player_speed = 1
-def speed2():
-    global player_speed
-    player_speed = 2
-def speed3():
-    global player_speed
-    player_speed = 3
-def speed4():
-    global player_speed
-    player_speed = 4
-def speed5():
-    global player_speed
-    player_speed = 5
-def speed6():
-    global player_speed
-    player_speed = 6
-def speed7():
-    global player_speed
-    player_speed = 7
-def speed8():
-    global player_speed
-    player_speed = 8
-def speed9():
-    global player_speed
-    player_speed = 9
-def speedr():
-    global player_speed
-    player_speed = 15
-def speedmore():
-    global player_speed
-    player_speed = 30
-
-win.onkey(speed1,"1")
-win.onkey(speed2,"2")
-win.onkey(speed3,"3")
-win.onkey(speed4,"4")
-win.onkey(speed5,"5")
-win.onkey(speed6,"6")
-win.onkey(speed7,"7")
-win.onkey(speed8,"8")
-win.onkey(speed9,"9")
-win.onkey(speedr,"r")
-win.onkey(speedmore,"m")
-
-previous_time = datetime.datetime.now()
-dt = 0
-
-def process_events():
-    for player in players:
-        if player.state["direction"] == "up":
-            player.forward(player_speed)
-        if player.state["direction"] == "down":
-            player.back(player_speed)
-        if player.state["direction"] == "left":
-            player.left(player_speed)
-        if player.state["direction"] == "right":
-            player.right(player_speed)
-        
-    p1_pos_x = players[0].pos()[0]
-    p1_pos_y = players[0].pos()[1]
-    p2_pos_x = players[1].pos()[0]
-    p2_pos_y = players[1].pos()[1]
-    food_pos_x = characters[2].pos()[0]
-    food_pos_y = characters[2].pos()[1]
-
-    if players[0].bias >= abs(p1_pos_x - food_pos_x) and players[0].bias >= abs(p1_pos_y - food_pos_y):
-        current_size = players[0].get_size()
-        players[0].resize(current_size[0]+1,current_size[1]+1,current_size[2])
-        players[0].bias = players[0].bias + 20
-        characters[2].setposition(random.randrange(-1*screen_size[0]*0.5,screen_size[0]*0.5),\
-                          random.randrange(-1*screen_size[1]*0.5,screen_size[1]*0.5))   
-
-    if players[1].bias >= abs(p2_pos_x - food_pos_x) and players[1].bias >= abs(p2_pos_y - food_pos_y):
-        current_size = players[1].get_size()
-        players[1].resize(current_size[0]+1,current_size[1]+1,current_size[2])
-        players[1].bias = players[1].bias + 20
-        characters[2].setposition(random.randrange(-1*screen_size[0]*0.5,screen_size[0]*0.5),\
-                          random.randrange(-1*screen_size[1]*0.5,screen_size[1]*0.5)) 
-
-    global previous_time
-    global dt
-    global previous_text
-
-    current_time = datetime.datetime.now()
-    delta = current_time - previous_time
-    dt = dt + delta.total_seconds()*1000
-    previous_time = current_time
-
-    if dt > 2000:
-        print(delta)
-        dt = 0
-
-        if previous_text != None:
-            previous_text.clear()
-
-    win.ontimer(process_events)
+    def process_events(self):
+        self.state.update()
+        self.state.update_players_position()
+        self.state.update_collisions()
+            
+        self.win.ontimer(self.process_events,16)
     
-process_events()
+    def run(self):
+        self.process_events()
+        
+        self.win.listen()
+        self.win.mainloop()
 
-win.listen()
-win.mainloop()
-
+turtle_game = TurtleGame()
+turtle_game.run()
 
